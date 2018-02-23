3f5a7716397efa3c8ec8e2c17a8dfd1b6e9d4e5a
dmitriy <dmitriy.mgdn@mail.ru>
2018-02-23 12:57:01 +0300

    LargeUnsignedNumber class: amended add() and expanded test; amended compareWith() and expanded test; added tests for the constructors; amended constructors; added minus() and a test to it.

--- Tests/sample/LargeUnsignedNumberTest.java
+++ Tests/sample/LargeUnsignedNumberTest.java
@@ -6,6 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LargeUnsignedNumberTest {
     @Test
+    void LargeUnsignedNumber() {
+        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> new LargeUnsignedNumber("0123456789"));
+        thrown = assertThrows(IllegalArgumentException.class, () -> new LargeUnsignedNumber(""));
+        thrown = assertThrows(IllegalArgumentException.class, () -> new LargeUnsignedNumber("12345678-9"));
+
+        assertEquals("12345678901234567890", new LargeUnsignedNumber("12345678901234567890").toString());
+    }
+
+    @Test
     void compareWith() {
         assertEquals("more",
                 new LargeUnsignedNumber("123456789123456789")
@@ -22,9 +31,20 @@ class LargeUnsignedNumberTest {
         assertEquals("equal",
                 new LargeUnsignedNumber("123456789123456789")
                 .compareWith(new LargeUnsignedNumber("123456789123456789")));
-        assertEquals("equal",
-                new LargeUnsignedNumber("0")
-                        .compareWith(new LargeUnsignedNumber("0")));
+        assertEquals("more",
+                new LargeUnsignedNumber("20123456789")
+                        .compareWith(new LargeUnsignedNumber("10123456789")));
+
+        int limit = 1000;
+        for (int i = 0; i <= limit; i++) {
+            for (int j = 0; j <= limit; j++) {
+                String comparison = (i - j < 0)? "less" : (i - j > 0)? "more" : "equal";
+
+                assertEquals(comparison,
+                        new LargeUnsignedNumber(String.valueOf(i))
+                                .compareWith(new LargeUnsignedNumber(String.valueOf(j))));
+            }
+        }
     }
 
     @Test
@@ -35,7 +55,38 @@ class LargeUnsignedNumberTest {
                 new LargeUnsignedNumber("0").add(new LargeUnsignedNumber("0")).toString());
         assertEquals("1111122222",
                 new LargeUnsignedNumber("11111").add(new LargeUnsignedNumber("1111111111")).toString());
+        assertEquals("1000999998",
+                new LargeUnsignedNumber("999999999").add(new LargeUnsignedNumber("999999")).toString());
+        assertEquals("1999999998",
+                new LargeUnsignedNumber("999999999").add(new LargeUnsignedNumber("999999999")).toString());
+
+        int limit = 1000;
+        for (int i = 0; i <= limit; i++) {
+            for (int j = 0; j <= limit; j++) {
+                assertEquals(String.valueOf(i + j),
+                        new LargeUnsignedNumber(String.valueOf(i))
+                                .add(new LargeUnsignedNumber(String.valueOf(j))).toString());
+            }
+        }
     }
 
+    @Test
+    void minus() {
+        assertEquals("12345678900000000000",
+                new LargeUnsignedNumber("12345678901234567890").minus(new LargeUnsignedNumber("1234567890")).toString());
+
+        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> {
+            new LargeUnsignedNumber("123").minus(new LargeUnsignedNumber("1234"));
+        });
+
+        int limit = 1000;
+        for (int i = 0; i <= limit; i++) {
+            for (int j = 0; j <= i; j++) {
+                assertEquals(String.valueOf(i - j),
+                        new LargeUnsignedNumber(String.valueOf(i))
+                                .minus(new LargeUnsignedNumber(String.valueOf(j))).toString(), String.valueOf(i) + ", " + String.valueOf(j));
+            }
+        }
+    }
 
 }
--- src/sample/LargeUnsignedNumber.java
+++ src/sample/LargeUnsignedNumber.java
@@ -1,15 +1,21 @@
 package sample;
 
 import static java.lang.Math.*;
+import static java.util.stream.IntStream.rangeClosed;
 
 class LargeUnsignedNumber {
-
     private String numberInStr;
 
     private int length;
 
     LargeUnsignedNumber(String number) {
         if (number.isEmpty()) throw new IllegalArgumentException("String in empty");
+        if (number.charAt(0) == '0' && number.length() > 1) throw new IllegalArgumentException("Incorrect number");
+        for (int i = 0; i < number.length(); i++) {
+            char charInNumber = number.charAt(i);
+            if (rangeClosed('0', '9').noneMatch((digit) -> digit == charInNumber))
+                throw new IllegalArgumentException("Incorrect number");
+        }
 
         this.numberInStr = new StringBuffer(number).reverse().toString();
 
@@ -26,13 +32,15 @@ class LargeUnsignedNumber {
         } else if (this.length < other.length) {
             return "less";
         } else {
-            if (numberInStr.charAt(0) > other.numberInStr.charAt(0)) {
-                return "more";
-            }
-            else if (numberInStr.charAt(0) < other.numberInStr.charAt(0)) {
-                return "less";
+            for (int i = this.length - 1; i >= 0; i--) {
+                if (numberInStr.charAt(i) > other.numberInStr.charAt(i)) {
+                    return "more";
+                } else if (numberInStr.charAt(i) < other.numberInStr.charAt(i)) {
+                    return "less";
+                }
             }
         }
+
         return "equal";
     }
 
@@ -48,9 +56,9 @@ class LargeUnsignedNumber {
             int digit1 = Integer.parseInt(String.valueOf(this.numberInStr.charAt(i)));
             int digit2 = Integer.parseInt(String.valueOf(other.numberInStr.charAt(i)));
 
-            int digitSum = digit1 + digit2;
+            int digitSum = digit1 + digit2 + remainder;
 
-            int digit = digitSum % 10 + remainder;
+            int digit = digitSum % 10;
 
             newNumber.append(String.valueOf(digit));
 
@@ -58,13 +66,55 @@ class LargeUnsignedNumber {
         }
 
         for (int i = shortestLength; i < longestLength; i++) {
-            LargeUnsignedNumber biggestNum = (this.length > other.length)? this : other;
-            int digit = Integer.parseInt(String.valueOf(biggestNum.numberInStr.charAt(i)));
+            LargeUnsignedNumber biggestNum = (this.length > other.length) ? this : other;
 
-            newNumber.append(String.valueOf(digit));
+            int digitSum = Integer.parseInt(String.valueOf(biggestNum.numberInStr.charAt(i))) + remainder;
+
+            newNumber.append(String.valueOf(digitSum % 10));
+
+            remainder = digitSum / 10;
         }
 
+        if (remainder != 0) newNumber.append("1");
         return new LargeUnsignedNumber(newNumber.reverse().toString());
+    }
+
+    LargeUnsignedNumber minus(LargeUnsignedNumber other) {
+        String comparison = this.compareWith(other);
+        if (comparison.equals("less"))
+            throw new IllegalArgumentException("result is negative: " + this.toString() + " - " + other.toString());
+        else if (comparison.equals("equal")) return new LargeUnsignedNumber("0");
+
+        StringBuilder newNumber = new StringBuilder("");
+
+        int remainder = 0;
+
+        for (int i = 0; i < other.length; i++) {
+            int digitDifference = this.numberInStr.charAt(i) - other.numberInStr.charAt(i) + remainder;
+
+            int digit = (digitDifference < 0) ? digitDifference + 10 : digitDifference;
+
+            newNumber.append(String.valueOf(digit));
+
+            remainder = (digitDifference < 0) ? -1 : 0;
+        }
+
+        for (int i = other.length; i < this.length; i++) {
+            int digitDifference = Integer.valueOf(String.valueOf(this.numberInStr.charAt(i))) + remainder;
+
+            int digit = (digitDifference < 0) ? digitDifference + 10 : digitDifference;
+
+            newNumber.append(String.valueOf(digit));
+
+            remainder = (digitDifference < 0) ? -1 : 0;
+        }
+
+        String newNumberInStr = newNumber.reverse().toString();
+        while (newNumberInStr.charAt(0) == '0') {
+            newNumberInStr = newNumberInStr.substring(1);
+        }
+
+        return new LargeUnsignedNumber(newNumberInStr);
     }
 
 
